97bf225f2d1da756e1f10aec316927af6a359dab
331uw13 <69628281+331uw13@users.noreply.github.com>
2021-04-14 21:13:39 +0300

    Delete def.h

--- src/def.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEF_H
-#define DEF_H
-
-typedef unsigned char        UINT8;
-typedef unsigned short int   UINT16;
-typedef unsigned int         UINT32;
-typedef unsigned long int    UINT64;
-
-#endif
